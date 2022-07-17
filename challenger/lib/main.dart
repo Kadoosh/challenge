4efa0a7634b471a33e2f86133fa71f904cfde0f1
Kadoosh <eletroriver@gmail.com>
2022-07-17 11:18:58 -0300

    update1.0

--- challenger/lib/main.dart
+++ challenger/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:challenger/src/screens/presentation.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
